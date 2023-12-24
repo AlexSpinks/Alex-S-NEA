909b74d1c21cc1a4f81012315475237e626aaafd
darthsidious06 <48269639+darthsidious06@users.noreply.github.com>
2023-12-24 22:23:56 +0000

    Update TypewriterAssistant.cs

--- Assets/Scripts/TypewriterAssistant.cs
+++ Assets/Scripts/TypewriterAssistant.cs
@@ -10,6 +10,9 @@ public class TypewriterAssistant : MonoBehaviour
         TypeWriter.add("Welcome to the tutorial");
         TypeWriter.add("Today we will learn how to play the game");
         TypeWriter.add("Press the W,A,S,D keys in order to move Up,Down,Left,Right respectively");
+        TypeWriter.add("Press the spacebar key in order to attack");
+        TypeWriter.add("running into health potions will heal you by 1 health point");
+        TypeWriter.add("running into good coins will increase your score");
 
         TypeWriter.Activate();
     }
