f7bf517ac64c82b0c57af63f4d4e6b6eb4fc8dfe
darthsidious06 <48269639+darthsidious06@users.noreply.github.com>
2023-12-24 22:20:37 +0000

    Update MapController.cs

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -101,19 +101,6 @@ public class MapController : MonoBehaviour
         AstarPath.active.Scan();
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
         // Start is called before the first frame update
         void Start()
         {
